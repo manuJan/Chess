980859047de98bf4688b355cef0924da0539f2b3
Javier Juste <javier.juste@atos.net>
2017-05-26 11:07:53 +0200

    deleted

--- DataBase/GRTLoadModel.bat
+++
@@ -1,7 +0,0 @@
-
-BCP %4.DBO.ZZT016_AREA in %1\data\ZZT016_AREA.txt  /n /c /C ACP /U sa /P %3 /S %2
-BCP %4.DBO.ZZT516_AREA in %1\data\ZZT516_AREA.txt  /n /c /C ACP /w /U sa /P %3 /S %2
-
-BCP %4.DBO.ZZT020_TYPE_TOP in %1\data\ZZT020_TYPE_TOP.txt  /n /c /C ACP /U sa /P %3 /S %2
-BCP %4.DBO.ZZT520_TYPE_TOP in %1\data\ZZT520_TYPE_TOP.txt  /n /c /C ACP /w /U sa /P %3 /S %2
-
